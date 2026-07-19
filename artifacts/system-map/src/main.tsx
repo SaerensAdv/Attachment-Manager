4dfc8efe343f07a0e8ec0aae280ba2e931c35934
SaerensAdv <axel@saerensadvertising.com>
2026-07-19 15:46:18 +0200

    feat(clients): expose operational client activity in Atlas

--- artifacts/system-map/src/main.tsx
+++ artifacts/system-map/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./pages/RunsGovernance.css";
+import "./pages/ClientOperations.css";
 
 declare const __ATLAS_BUILD_SHA__: string;
 (globalThis as typeof globalThis & { __ATLAS_FRONTEND_SHA__?: string }).__ATLAS_FRONTEND_SHA__ = __ATLAS_BUILD_SHA__;
